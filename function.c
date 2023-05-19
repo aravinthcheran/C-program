aa52fa7bb2840c753e04141bf9827b87965a0b1e
Aravinth Cheran <115545089+aravinthcheran@users.noreply.github.com>
2023-05-19 09:01:55 +0000

    ss

---
+++ function.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int sum(int a,int b)
+{
+    int s=a+b;
+    return s;
+}
+int main()
+{
+    printf("Sum of 2 and 3 is %d",sum(2,3));
+}
---
+++ function1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int hello(char a[100])
+{
+    printf("Hello World %s",a);
+}
+int main()
+{
+    hello("cheran");
+    return 0;
+}
