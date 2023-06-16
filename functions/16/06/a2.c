46439b751f82273801dbcce6f043ac60ede2195b
Aravinth Cheran <115545089+aravinthcheran@users.noreply.github.com>
2023-06-16 09:04:24 +0000

    ff

---
+++ functions/16/06/a2.c
@@ -0,0 +1,13 @@
+//write the program that gets the sentence from the user and stores it dynamically an d finds teh length of the sentence using strlen() function
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char *p;
+    p=(char *)calloc(500,sizeof(char));
+    printf("Enter the sentence: ");
+    scanf("%[^\n]s",p);
+    printf("The sentence is: %s\n",p);
+    printf("The length of the sentence is: %d\n",strlen(p));
+}
--- functions/16/06/dma.c
+++ functions/16/06/dma.c
@@ -3,17 +3,36 @@
 int main()
 {
     int *p,n;
-    p=(int *)malloc(sizeof(int)*n);
+    p=(int *)calloc(n,sizeof(int));
     printf("Enter the number of elements: ");
     scanf("%d",&n);
     printf("Enter the elements: ");
     for(int i=0;i<n;i++)
     {
-        scanf("%d",p+i);
+        scanf("%d",&p[i]);
     }
     printf("The elements are: ");
     for(int i=0;i<n;i++)
     {
         printf("%d ",*(p+i));
     }
+    int m;
+    printf("The new size of the array is: ");
+    scanf("%d",&m);
+    p=(int *)realloc(p,(sizeof(int)*n)-(sizeof(int)*m));
+    printf("The elements are: ");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",*(p+i));
+    }
+    int o;
+    printf("The new size of the array is: ");
+    scanf("%d",&o);
+    p=(int *)realloc(p,(sizeof(int)*m)+(sizeof(int)*o));
+    printf("The elements are: ");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",*(p+i));
+    }
+    
 }
