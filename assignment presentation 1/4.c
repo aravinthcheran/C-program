2075530298bb29f035dd9ad8a42bb82039630cab
Aravinth Cheran <115545089+aravinthcheran@users.noreply.github.com>
2023-07-06 05:50:15 +0000

    assingment presentation

--- assignment presentation 1/4.c
+++ assignment presentation 1/4.c
@@ -1,60 +1,51 @@
-/*Write a function that accepts a string and returns the longest palindrome substring in the string.*/
 #include<stdio.h>
 #include<string.h>
 #define MAX_LENGTH 100
-
-int isPalindrome(char str[], int start, int end)
+int palindrome(char *str, int start, int end)
 {
     while (start < end)
     {
         if (str[start] != str[end])
-            return 0;
+            break;
         start++;
         end--;
     }
-    return 1;
+    if(start >= end)
+        return 1;
+    else
+        return 0;
 }
-
-char* longestPalindromeSubstring(char str[])
+char* longestpalindrome(char *str)
 {
+    static char result[MAX_LENGTH];
     int len = strlen(str);
-    int maxLength = 0;
+    int maxlength = 0;
     int start = 0;
-
     for (int i = 0; i < len; i++)
     {
         for (int j = i; j < len; j++)
         {
-            if (isPalindrome(str, i, j))
+            if (palindrome(str, i, j))
             {
-                int currentLength = j - i + 1;
-                if (currentLength > maxLength)
+                int currentlength = j - i + 1;
+                if (currentlength > maxlength)
                 {
-                    maxLength = currentLength;
+                    maxlength = currentlength;
                     start = i;
                 }
             }
         }
     }
-
-    char* result = malloc(sizeof(char) * (maxLength + 1));
-    strncpy(result, str + start, maxLength);
-    result[maxLength] = '\0';
-
+    strncpy(result, str + start, maxlength);
+    result[maxlength] = '\0';
     return result;
 }
-
 int main()
 {
     char str[MAX_LENGTH];
     printf("Enter the string: ");
     scanf("%s", str);
-
-    char* longestPalindrome = longestPalindromeSubstring(str);
-
-    printf("The longest palindrome substring is: %s\n", longestPalindrome);
-
-    free(longestPalindrome);
-
+    char* longest = longestpalindrome(str);
+    printf("\nThe longest palindrome substring is: %s\n", longest);
     return 0;
 }
