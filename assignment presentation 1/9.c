f5b3727f5982b361f6386fa3b6ce6e83e78bbda1
Aravinth Cheran <115545089+aravinthcheran@users.noreply.github.com>
2023-07-11 04:00:38 +0000

    s

--- assignment presentation 1/9.c
+++ assignment presentation 1/9.c
@@ -1,57 +1,60 @@
-/*Write a function that accepts a pointer to a 2D array of integers and rotates the array 90 degrees
-clockwise in-place.*/
 #include<stdio.h>
 #include<stdlib.h>
-int rotate(int r,int c,int (*arr)[c]);
+
+void rotate(int r, int c, int (*arr)[c]);
+
 int main()
 {
     printf("Enter the number of rows: ");
     int r;
-    scanf("%d",&r);
+    scanf("%d", &r);
     printf("Enter the number of columns: ");
     int c;
-    scanf("%d",&c);
-    int (*arr)[c]=malloc(sizeof(int)*r*c);
-    printf("Enter the elements of the array: ");
-    for(int i=0;i<r;i++)
+    scanf("%d", &c);
+    int (*arr)[c] = malloc(sizeof(int) * r * c);
+    printf("Enter the elements of the array: \n");
+    for(int i = 0; i < r; i++)
     {
-        for(int j=0;j<c;j++)
+        for(int j = 0; j < c; j++)
         {
-            scanf("%d",&arr[i][j]);
+            scanf("%d", &arr[i][j]);
         }
     }
-    rotate(r,c,arr);
+    rotate(r, c, arr);
+    printf("The rotated array is: \n");
+    for(int i = 0; i < c; i++)
+    {
+        for(int j = 0; j < r; j++)
+        {
+            printf("%d ", arr[j][i]);
+        }
+        printf("\n");
+    }
 }
-int rotate(int r,int c, int (*arr)[c])
+
+void rotate(int r, int c, int (*arr)[c])
 {
-    int arr1[c][r];
-    for(int i=0;i<r;i++)
+    for(int i = 0; i < r; i++)
     {
-        for(int j=0;j<c;j++)
+        for(int j = i + 1; j < c; j++)
         {
-            arr1[j][i]=arr[i][j];
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
         }
     }
-    for(int i=0;i<c;i++)
+    for(int i = 0; i < r; i++)
     {
-        int f=0,l=r-1;
-        while(f<l)
+        int f = 0;
+        int l = c - 1;
+
+        while(f < l)
         {
-            int temp=arr1[i][f];
-            arr1[i][f]=arr1[i][l];
-            arr1[i][l]=temp;
+            int temp = arr[i][f];
+            arr[i][f] = arr[i][l];
+            arr[i][l] = temp;
             f++;
             l--;
         }
-    }
-    printf("The rotated array is: \n");
-    for(int i=0;i<r;i++)
-    {
-        for(int j=0;j<c;j++)
-        {
-            arr[i][j]=arr1[i][j];
-            printf("%d ",arr[i][j]);
-        }
-        printf("\n");
     }
 }
