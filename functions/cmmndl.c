fe422febaab8b0bbb15759ec307b58a86adf221d
Aravinth Cheran <115545089+aravinthcheran@users.noreply.github.com>
2023-07-17 15:05:38 +0000

    j

--- functions/cmmndl.c
+++ functions/cmmndl.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main(int argc,char* argv[])
 {
+       // Display the program name
     printf("Program Name: %s\n",argv[0]);
+    // Display the arguments
     printf("Arguments are:\n");
     for(int i=1;i<argc;i++)
     {
