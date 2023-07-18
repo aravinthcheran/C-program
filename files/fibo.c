8795a1ca57b506c02cc166a357bff2639d8dc3f4
Aravinth Cheran <115545089+aravinthcheran@users.noreply.github.com>
2023-07-18 17:44:08 +0000

    gg

--- files/fibo.c
+++ files/fibo.c
@@ -19,16 +19,11 @@ int main()
     int n,p;
     printf("Enter the number of terms: ");
     scanf("%d",&n);
-    int fib[n];
-    for(int i=0;i<n;i++)
-    {
-        fib[i]=fibo(i+1);
-    }
     FILE *fp;
     fp=fopen("fibo.txt","w");
-    for(int i=0;i<n;i++)
+    for(int i=1;i<=n;i++)
     {
-        fprintf(fp,"%d ",fib[i]);
+        fprintf(fp,"%d\n",fibo(i));
     }
     fclose(fp);
     fp=fopen("fibo.txt","r");
@@ -40,7 +35,7 @@ int main()
     for(int i=0;i<n;i++)
     {
         fscanf(fp,"%d ",&p);
-        printf("%d ",p);
+        printf("%d\n",p);
     }
     
 }
